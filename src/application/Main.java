e1a5e22233068522954ee5f5703b976ca29bfa6d
arguelles-norlan <norlancarguelles@iskolarngbayan.pup.edu.ph>
2025-05-18 05:26:27 +0800

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 
 public class Main extends Application {
 	@Override
@@ -13,7 +14,10 @@ public class Main extends Application {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("SignUp.fxml"));
 			Scene scene = new Scene(root,1024,700);
 			scene.getStylesheets().add(getClass().getResource("SignUp.css").toExternalForm());
+			primaryStage.getIcons().add(new Image("file:QuirxImages/LogoYellow.png"));
+			primaryStage.setTitle("Quirx");
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
