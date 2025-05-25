8becc696ab3810f6610ef086c813e773681cb0b9
caya-karlchristian <karlcaya0704@gmail.com>
2025-05-26 00:18:49 +0800

    Generate javadoc for SignUp.java

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"application","l":"LogIn"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"application","l":"SignUp"}];updateSearchResults();
