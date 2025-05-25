16ddac59363824528f0fa3f6def4cbce8c0a6011
caya-karlchristian <karlcaya0704@gmail.com>
2025-05-26 01:48:44 +0800

    Generate javadoc for all classes

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"application","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"application","l":"LogIn"},{"p":"application","l":"Main"},{"p":"application","l":"MenuController"},{"p":"application","l":"MyProfileController"},{"p":"application","l":"Task.Priority"},{"p":"application","l":"SignUp"},{"p":"application","l":"Task.Status"},{"p":"application","l":"Task"},{"p":"application","l":"WorkspaceController"}];updateSearchResults();
